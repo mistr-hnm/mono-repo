5989409d7154ca3a281b050fb3ff37fd52b828c2
hnm <mistrhnm@gmail.com>
2025-07-04 01:16:31 +0530

    New: Integration of workspace for FE and BE

---
+++ apps/myschool-fe/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css' 
+import { DataTableDemo } from './modules/courses/course'
+import { StudentTable } from './modules/student/student'
+
+function App() { 
+ 
+  return (
+    <>  
+    {/* <DataTableDemo />  */}
+    <StudentTable /> 
+    
+    </>
+  )
+}
+
+export default App
