980b8b5049737845e84b96b37120f4d220389044
hnm <mistrhnm@gmail.com>
2025-07-28 00:57:03 +0530

    Fix: signup path

--- apps/myschool-fe/src/services/mutation/user.ts
+++ apps/myschool-fe/src/services/mutation/user.ts
@@ -7,7 +7,7 @@ const url = `${import.meta.env.VITE_BE_BASE_URL}`;
 export function useSignupUser() {
   return useMutation({
         mutationFn : async (body : UserBody) => {
-            const response = await axiosInstance.post(`${url}/users`, body)
+            const response = await axiosInstance.post(`${url}/users/signup`, body)
             return response.data;
         }
     })
