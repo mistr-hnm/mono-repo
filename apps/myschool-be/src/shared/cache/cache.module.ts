793130a375d971d7afd3baf284f505a4674fad69
hnm <mistrhnm@gmail.com>
2025-07-31 23:51:34 +0530

    Update: redis package

--- apps/myschool-be/src/shared/cache/cache.module.ts
+++ apps/myschool-be/src/shared/cache/cache.module.ts
@@ -2,7 +2,7 @@ import { CacheModule } from "@nestjs/cache-manager";
 import { Module } from "@nestjs/common";
 import { CacheService } from "./cache.service";
 import { ConfigModule, ConfigService } from "@nestjs/config";
-import { createKeyv } from '@keyv/redis';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
     imports: [
@@ -10,9 +10,10 @@ import { createKeyv } from '@keyv/redis';
             isGlobal: true,
             imports: [ConfigModule],
             useFactory: async (configService: ConfigService) => {
-                const url = configService.get('REDIS_URL');
                 return {
-                    stores: [createKeyv(url)],
+                    store: redisStore,
+                    url: configService.get('REDIS_URL'),
+                    ttl: 600, // 10 minutes default
                 };
             },
             inject: [ConfigService]
